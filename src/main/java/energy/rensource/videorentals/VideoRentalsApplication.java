38dd39307fb06b6d7599d3e132a0367657b876b2
Remy Ohajinwa <remy.ohajinwa@interswitchgroup.com>
2021-06-01 22:35:11 +0100

    Initial commit

---
+++ src/main/java/energy/rensource/videorentals/VideoRentalsApplication.java
@@ -0,0 +1,13 @@
+package energy.rensource.videorentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoRentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/energy/rensource/videorentals/VideoRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package energy.rensource.videorentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoRentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
